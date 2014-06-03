698fbacc93f609aee2ffab6ab2275a8ccb547b57
Andreas Janker <janker@ett.fim.uni-passau.de>
2014-06-03 15:43:32 +0200

    made prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+path=$(cd "$(dirname "$0")"; pwd)
+
+filesToProcess() {
+  local listFile=busybox_files
+  cat $listFile
+}
+
+flags="-x CONFIG_ \
+  -c $path/redhat.properties \
+  --include $path/config.h \
+  -I $path/busybox-1.18.5/include \
+  --featureModelDimacs $path/BB_fm.dimacs \
+  --recordTiming --parserstatistics --lexNoStdout \
+  -U HAVE_LIBDMALLOC \
+  -DCONFIG_FIND \
+  -U CONFIG_FEATURE_WGET_LONG_OPTIONS \
+  -U ENABLE_NC_110_COMPAT \
+  -U CONFIG_EXTRA_COMPAT \
+  -D_GNU_SOURCE"
+
+#tunitgen
+
+tunitgen=" --bdd \
+  --study busybox --serializeAST --writePI --interface --debugInterface"
+
+
+filesToProcess|while read i; do
+         echo "Analysing $path/busybox-1.18.5/$i.c"
+         echo "With settings: $tunitgen $flags"
+         ../Morpheus/morpheus.sh $path/busybox-1.18.5/$i.c $tunitgen $flags
+	 done
+
+interfacegen="--study busybox  --writeProjectInterface"
+
+echo "Generate interfaces"
+
+../Morpheus/morpheus.sh $path/busybox-1.18.5/$i.c $interfacegen
+
+refgen="--bdd \
+  --study busybox --reuseAST --refPrep --refLink $path/CLinking.interface"
+
+filesToProcess|while read i; do
+         echo "Analysing $path/busybox-1.18.5/$i.c"
+         echo "With settings: $refgen $flags"
+         ../Morpheus/morpheus.sh $path/busybox-1.18.5/$i.c $refgen $flags
+         done
+
+
+
