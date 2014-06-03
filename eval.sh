37d45d6cda4bb5c3600f3456a9c3fdb57ac176f0
Andreas Janker <janker@ett.fim.uni-passau.de>
2014-06-03 15:27:58 +0200

    updated eval.sh to new project setup

--- eval.sh
+++ eval.sh
@@ -26,6 +26,6 @@ flags=" --bdd \
 filesToProcess|while read i; do
          echo "Analysing $path/busybox-1.18.5/$i.c"
          echo "With settings: $flags"
-         ../TypeChef/cRefactor.sh $path/busybox-1.18.5/$i.c $flags
+         ../Morpheus/morpheus.sh $path/busybox-1.18.5/$i.c $flags
 	 done
 
