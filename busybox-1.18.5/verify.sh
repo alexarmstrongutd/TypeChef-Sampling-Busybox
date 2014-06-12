e76034495dff24c40cb2a1b656b7da12603872e3
Andreas Janker <janker@ett.fim.uni-passau.de>
2014-06-12 15:12:38 +0200

    cleanup before each test run

--- busybox-1.18.5/verify.sh
+++ busybox-1.18.5/verify.sh
@@ -18,6 +18,8 @@ filesToProcess|while read config; do
         cp $config .config 
         ./build.sh | tee "$1"/"$i"_ref.build
         cd testsuite
+        git clean -f
+        git checkout .
         ./runtest | tee "$1"/"$i"_ref.test
         cd ..
         i=`expr $i + 1`
@@ -36,6 +38,8 @@ filesToProcess|while read config; do
         cp $config .config 
         ./build.sh | tee "$1"/"$i"_org.build
         cd testsuite
+        git clean -f
+        git checkout .
         ./runtest | tee "$1"/"$i"_org.test
         cd ..        
         i=`expr $i + 1`
