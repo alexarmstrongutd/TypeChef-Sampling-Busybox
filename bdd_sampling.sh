500ad339f4c79647e16590f83212c4119c895b38
Andreas Janker <janker@ravel.fim.uni-passau.de>
2016-02-22 19:53:19 +0100

    updated scripts

--- bdd_sampling.sh
+++ bdd_sampling.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-path=$(cd "$(dirname "$0")"; pwd)
-path=$(echo $path | sed s/scratch/local/g)
+#SBATCH -D /scratch/janker/busybox/Morpheus-BusyBoxEvaluation
+#SBATCH --job-name=typechef
+#SBATCH -p chimaira
+#SBATCH -A spl
+#SBATCH --get-user-env
+#SBATCH -n 1
+#SBATCH -c 2
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=janker@fim.uni-passau.de
+#SBATCH --mem_bind=local
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+#SBATCH --time=12:00:00
+#SBATCH --array=0-570
+
+path=$(pwd)
+#path=$(echo $path | sed s/scratch/local/g)
 
 filesToProcess() {
   local listFile=casestudy/busybox_files
@@ -21,12 +36,13 @@ flags=" --bdd --reuseAST \
   -U ENABLE_NC_110_COMPAT \
   -U CONFIG_EXTRA_COMPAT \
   -D_GNU_SOURCE"
+configId=${SLURM_ARRAY_TASK_ID}
 
+i=`cat casestudy/busybox_files | head -n $((configId + 1)) | tail -n1`
 
-filesToProcess|while read i; do
-         echo "Analysing $path/busybox-1.18.5/$i.c"
-         echo "With settings: $flags"
-         sbatch -p chimaira  -A spl -n 1 -c 2 --time=06:00:00  --mem_bind=local --output=/dev/null --error=/dev/null  /home/janker/clusterScripts/bb_sampling.sh $path/busybox-1.18.5/$i.c $flags
-	 # timeout 3h ../TypeChef/typechef.sh $path/busybox-1.18.5/$i.c $flags
-	 done
+     echo "Analysing $path/busybox-1.18.5/$i.c"
+     echo "With settings: $flags"
+     ./sampling.sh $path/busybox-1.18.5/$i.c $flags 
+     #sbatch -p chimaira  -A spl -n 1 -c 2 --time=06:00:00  --mem_bind=local --output=/dev/null --error=/dev/null  /home/janker/clusterScripts/bb_sampling.sh $path/busybox-1.18.5/$i.c $flags
+     # timeout 3h ../TypeChef/typechef.sh $path/busybox-1.18.5/$i.c $flags
 
--- bdd_vaa.sh
+++ bdd_vaa.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-path=$(cd "$(dirname "$0")"; pwd)
-path=$(echo $path | sed s/scratch/local/g)
+#SBATCH -D /scratch/janker/busybox/Morpheus-BusyBoxEvaluation
+#SBATCH --job-name=typechef
+#SBATCH -p chimaira
+#SBATCH -A spl
+#SBATCH --get-user-env
+#SBATCH -n 1
+#SBATCH -c 2
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=janker@fim.uni-passau.de
+#SBATCH --mem_bind=local
+#SBATCH --output=/dev/null
+#SBATCH --error=/dev/null
+#SBATCH --time=12:00:00
+#SBATCH --array=0-570
 
-filesToProcess() {
-  local listFile=casestudy/busybox_files
-  cat $listFile
-}
+path=$(pwd)
 
-flags=" --bdd --serializeAST \
-  -A doublefree -A xfree -A uninitializedmemory -A casetermination -A danglingswitchcode -A checkstdlibfuncreturn -A deadstore -A interactiondegree \
+configId=${SLURM_ARRAY_TASK_ID}
+i=`cat casestudy/busybox_files | head -n $((configId + 1)) | tail -n1`
+
+flags=" --bdd --reuseAST \
+  -A cfginnonvoidfunction -A doublefree -A xfree -A uninitializedmemory -A casetermination -A danglingswitchcode -A checkstdlibfuncreturn -A deadstore -A interactiondegree \
   -x CONFIG_ \
   -c $path/casestudy/redhat.properties \
   --include $path/casestudy/config.h \
@@ -24,10 +36,9 @@ flags=" --bdd --serializeAST \
   -D_GNU_SOURCE"
 
 
-filesToProcess|while read i; do
          echo "Analysing $path/busybox-1.18.5/$i.c"
          echo "With settings: $flags"
-         sbatch -p chimaira  -A spl -n 1 -c 2 --time=06:00:00  --mem_bind=local --output=/dev/null --error=/dev/null  /home/janker/clusterScripts/bb_vaa.sh $path/busybox-1.18.5/$i.c $flags
+         /scratch/janker/TypeChef/typechef.sh $path/busybox-1.18.5/$i.c $flags
+	 #sbatch -p chimaira  -A spl -n 1 -c 2 --time=06:00:00  --mem_bind=local --output=/dev/null --error=/dev/null  /home/janker/clusterScripts/bb_vaa.sh $path/busybox-1.18.5/$i.c $flags
 	 # timeout 3h ../TypeChef/typechef.sh $path/busybox-1.18.5/$i.c $flags
-	 done
 
