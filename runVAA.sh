8006ea9d561089b2087e79f020b086ce09372245
Alexander Armstrong <alex@prolangs.utdallas.edu>
2020-03-24 10:56:50 -0500

    Made changes to get the analysis to run

--- runVAA.sh
+++ runVAA.sh
@@ -8,20 +8,26 @@ filesToProcess() {
   cat $listFile
 }
 
-flags=" --bdd --serializeAST \
+flags="--bdd  --serializeAST \
   -A cfginnonvoidfunction -A doublefree -A xfree -A uninitializedmemory -A casetermination -A danglingswitchcode -A checkstdlibfuncreturn -A deadstore -A interactiondegree \
   -x CONFIG_ \
   -c $path/casestudy/redhat.properties \
   --include $path/casestudy/config.h \
   -I $path/busybox-1.18.5/include \
+   --platfromHeader ../TypeChef-GNUCHeader/platform.h \
+  -I ../TypeChef-GNUCHeader/usr_include \
+  -I ../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include \
+  -I ../TypeChef-GNUCHeader/x86_64-linux-gnu/4.8/include-fixed \
+  -I ../TypeChef-GNUCHeader/x86_64-linux-gnu \
+  --openFeat ../TypeChef-GNUCHeader/openfeatures.txt \
   --featureModelDimacs $path/casestudy/BB_fm.dimacs  \
-  --recordTiming --parserstatistics --lexNoStdout \
-  -U HAVE_LIBDMALLOC \
-  -DCONFIG_FIND \
-  -U CONFIG_FEATURE_WGET_LONG_OPTIONS \
-  -U ENABLE_NC_110_COMPAT \
-  -U CONFIG_EXTRA_COMPAT \
-  -D_GNU_SOURCE"
+  --recordTiming --parserstatistics --lexNoStdout
+	-U HAVE_LIBDMALLOC \
+	-DCONFIG_FIND \
+	-U CONFIG_FEATURE_WGET_LONG_OPTIONS \
+	-U ENABLE_NC_110_COMPAT \
+	-U CONFIG_EXTRA_COMPAT \
+	-D_GNU_SOURCE"
 
 filesToProcess|while read i; do
          echo "Analysing $path/busybox-1.18.5/$i.c"
